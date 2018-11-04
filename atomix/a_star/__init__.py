d385d93fbb19eb9db2b2435ef3f92fb7b17f4126
Omar Salinas <omar.salinas1611@gmail.com>
2018-11-03 19:49:51 -0600

    Removed unused files

--- atomix/a_star/__init__.py
+++
@@ -1,8 +0,0 @@
-from a_star.nodetype import NodeType
-from a_star.node import Node
-from a_star.diagram import Diagram
-from a_star.priority_queue import PriorityQueue
-from a_star.astar import a_star
-
-
-_all_ = ["NodeType", "Node", "Diagram", "a_star"]
